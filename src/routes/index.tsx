a6a02f10eba886da1aa103d6f7ee5422a26f0912
Karina Borges <karina_borges@id.uff.br>
2022-11-06 17:17:25 -0300

    ✨ add authenticated routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { Box } from "native-base";
+
+import { useAuth } from "../hooks/useAuth";
+
+import { SignIn } from "../screens/SingIn";
 import { AppRoutes } from "./app.routes";
 
 export const Routes = () => {
+  const { user } = useAuth();
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="gray.900">
+      <NavigationContainer>
+        {user.name ? <AppRoutes /> : <SignIn />}
+      </NavigationContainer>
+    </Box>
   );
 };
--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,12 +1,12 @@
-import { Center, Icon, Text } from "native-base";
 import { Fontisto } from "@expo/vector-icons";
+import { Center, Icon, Text } from "native-base";
 
 import Logo from "../assets/logo.svg";
 import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
 
 export function SignIn() {
-  const { signIn, user } = useAuth();
+  const { signIn, isUserLoading } = useAuth();
 
   return (
     <Center flex={1} bgColor="gray.900" p={7}>
@@ -16,7 +16,11 @@ export function SignIn() {
         title="ENTRAR COM O GOOGLE"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
         mt={12}
-        onPress={signIn}
+        onPress={() => signIn()}
+        isLoading={isUserLoading}
+        _loading={{
+          _spinner: { color: "white" },
+        }}
       />
       <Text color="white" textAlign="center" mt={4}>
         Não utilizamos nenhuma informação além {"\n"}do seu e-mail para criação
